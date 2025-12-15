e48da214a679215b98ef2d3f882d80a73ce32c84
mayowa akinyele <mayowaakinyele@mac.speedport.ip>
2025-12-15 01:12:10 +0100

    test

--- data_lake.tf
+++ data_lake.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "ecommerce_raw" {
   bucket = "raw-data-ecommerce-bucket"
 }
+
+resource "aws_s3_bucket" "ecommerce_raw1" {
+  bucket = "raw-data-ecommerce-buckethhghdjfhwjrgh"
+}
