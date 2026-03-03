c987f7dbcad3bc9cf654d4abc0d2c2e64686af60
Jhoie <victoriousjvictor@gmail.com>
2026-03-03 11:01:59 +0100

    Update vpc subnets

--- eu-central-1/production/vpc.tf
+++ eu-central-1/production/vpc.tf
@@ -1,76 +1,125 @@
 resource "aws_vpc" "federated-engineers-vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "172.16.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = merge(local.common_tags, { Name : "secure-production" })
+  tags = merge(local.common_tags, { Name = "secure-production" })
 }
 
-resource "aws_subnet" "private-subnet" {
-
+resource "aws_subnet" "public-a" {
   vpc_id            = aws_vpc.federated-engineers-vpc.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "172.16.0.0/21"
   availability_zone = "eu-central-1a"
 
-  tags = merge(local.common_tags, { Name : "secure-production-private" })
-}
-
-resource "aws_subnet" "public-subnet" {
-
-  vpc_id                  = aws_vpc.federated-engineers-vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = true
 
-  tags = merge(local.common_tags, { Name : "secure-production-public" })
+  tags = merge(local.common_tags, { Name = "secure-production-public-a" })
 }
 
-resource "aws_internet_gateway" "secure-production-igw" {
+resource "aws_subnet" "private-a" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  cidr_block        = "172.16.8.0/21"
+  availability_zone = "eu-central-1a"
+
+  tags = merge(local.common_tags, { Name = "secure-production-private-a" })
+}
+
+resource "aws_subnet" "public-b" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  cidr_block        = "172.16.16.0/21"
+  availability_zone = "eu-central-1b"
+
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, { Name = "secure-production-public-b" })
+}
+
+resource "aws_subnet" "private-b" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  cidr_block        = "172.16.24.0/21"
+  availability_zone = "eu-central-1b"
+
+  tags = merge(local.common_tags, { Name = "secure-production-private-b" })
+}
+
+resource "aws_subnet" "public-c" {
+  vpc_id                  = aws_vpc.federated-engineers-vpc.id
+  cidr_block              = "172.16.32.0/21"
+  availability_zone       = "eu-central-1c"
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, { Name = "secure-production-public-c" })
+}
+
+resource "aws_subnet" "private-c" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  cidr_block        = "172.16.40.0/21"
+  availability_zone = "eu-central-1c"
+
+  tags = merge(local.common_tags, { Name = "secure-production-private-c" })
+}
+
+resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.federated-engineers-vpc.id
 
-  tags = merge(local.common_tags, { Name : "secure-production-igw" })
+  tags = merge(local.common_tags, { Name = "vpc-internet-gateway" })
 }
 
-resource "aws_route_table" "private-rtb" {
+resource "aws_route_table" "public-subnet-rtb" {
   vpc_id = aws_vpc.federated-engineers-vpc.id
+  tags   = merge(local.common_tags, { Name = "vpc-public-subnet-rtb" })
 
-  tags = merge(local.common_tags, { Name : "secure-production-private-rt" })
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
 }
 
-resource "aws_route_table_association" "private_subnet_associations" {
-
-  subnet_id      = aws_subnet.private-subnet.id
-  route_table_id = aws_route_table.private-rtb.id
+resource "aws_route_table_association" "public-rtb-a" {
+  subnet_id      = aws_subnet.public-a.id
+  route_table_id = aws_route_table.public-subnet-rtb.id
 }
 
-resource "aws_route_table" "public-rtb" {
+resource "aws_route_table_association" "public-rtb-b" {
+  subnet_id      = aws_subnet.public-b.id
+  route_table_id = aws_route_table.public-subnet-rtb.id
+}
+
+resource "aws_route_table_association" "public-rtb-c" {
+  subnet_id      = aws_subnet.public-c.id
+  route_table_id = aws_route_table.public-subnet-rtb.id
+}
+
+resource "aws_route_table" "private-subnet-rtb" {
   vpc_id = aws_vpc.federated-engineers-vpc.id
-
-  tags = merge(local.common_tags, { Name : "secure-production-public-rt" })
+  tags   = merge(local.common_tags, { Name = "vpc-private-subnet-rtb" })
 }
 
-resource "aws_route_table_association" "public_subnet_associations" {
-
-  subnet_id      = aws_subnet.public-subnet.id
-  route_table_id = aws_route_table.public-rtb.id
+resource "aws_route_table_association" "private-rtb-a" {
+  subnet_id      = aws_subnet.private-a.id
+  route_table_id = aws_route_table.private-subnet-rtb.id
 }
 
-resource "aws_route" "public-route" {
-  route_table_id         = aws_route_table.public-rtb.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.secure-production-igw.id
+resource "aws_route_table_association" "private-rtb-b" {
+  subnet_id      = aws_subnet.private-b.id
+  route_table_id = aws_route_table.private-subnet-rtb.id
 }
 
-resource "aws_security_group" "secure-production-sg" {
-  name        = "secure-production-sg"
-  description = "Dedicated security group for the VPC"
+resource "aws_route_table_association" "private-rtb-c" {
+  subnet_id      = aws_subnet.private-c.id
+  route_table_id = aws_route_table.private-subnet-rtb.id
+}
+
+resource "aws_security_group" "sg" {
+  name        = "secure-sg"
+  description = "Security Group for the VPC"
   vpc_id      = aws_vpc.federated-engineers-vpc.id
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, { Name = "vpc-sg" })
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.secure-production-sg.id
+resource "aws_vpc_security_group_egress_rule" "sg-egress" {
+  security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
