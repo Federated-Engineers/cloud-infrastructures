4797801a594885d1f9fb08c8ea57d5498c7852e3
mayowa akinyele <mayowaakinyele@ip-192-168-2-196.eu-central-1.compute.internal>
2026-04-07 20:51:32 +0200

    add tags

--- eu-central-1/production/eks-node-group.tf
+++ eu-central-1/production/eks-node-group.tf
@@ -49,6 +49,8 @@
 #     max_unavailable = 1
 #   }
 
+#   tags = local.common_tags
+
 #   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
 #   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
 #   depends_on = [
