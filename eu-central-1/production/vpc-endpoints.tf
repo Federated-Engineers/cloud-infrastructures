cf3bb849753cef50bc0fb579421854467644e5cf
Musili-Adebayo <adebayomusili@gmail.com>
2026-06-11 16:52:27 +0100

    created vpc endpoint for access for for dag to access ssm and image and logs

--- eu-central-1/production/vpc-endpoints.tf
+++ eu-central-1/production/vpc-endpoints.tf
@@ -6,3 +6,82 @@ resource "aws_vpc_endpoint" "s3_gateway" {
 
   tags = merge(local.common_tags, { Name : "secure-production-s3-vpc-endpoint" })
 }
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  service_name      = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private-a.id,
+    aws_subnet.private-b.id,
+    aws_subnet.private-c.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.sg.id
+  ]
+
+  private_dns_enabled = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "secure-production-ssm-endpoint"
+    }
+  )
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  service_name      = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private-a.id,
+    aws_subnet.private-b.id,
+    aws_subnet.private-c.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.sg.id
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  service_name      = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private-a.id,
+    aws_subnet.private-b.id,
+    aws_subnet.private-c.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.sg.id
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id            = aws_vpc.federated-engineers-vpc.id
+  service_name      = "com.amazonaws.${var.region}.logs"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private-a.id,
+    aws_subnet.private-b.id,
+    aws_subnet.private-c.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.sg.id
+  ]
+
+  private_dns_enabled = true
+}
--- eu-central-1/production/vpc.tf
+++ eu-central-1/production/vpc.tf
@@ -123,3 +123,11 @@ resource "aws_vpc_security_group_egress_rule" "sg-egress" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+resource "aws_vpc_security_group_ingress_rule" "sg-ingress" {
+  security_group_id            = aws_security_group.sg.id
+  referenced_security_group_id = aws_security_group.sg.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+}
